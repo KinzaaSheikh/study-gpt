903240bad0ce0527259c2665fa9e1c1b7dd1136a
Kinza <kinzasheikhsiddiqui@gmail.com>
2025-09-18 00:17:08 +0500

    Started configuring LiteLLM for Gemini but so far its throwing "Module is not callable" error.

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,19 @@
+import os
+from dotenv import load_dotenv
 import chainlit as cl
+from agents import Agent, Runner, set_tracing_disabled
+from agents.extensions.models import litellm_provider, litellm_model
 
-@cl.on_message
-async def main(message: cl.Message):
-    # Our custom logic goes here...
-    # Send a fake response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+model = litellm_provider()
+
+@cl.on_chat_start
+async def start():
+    agent = Agent(
+        name = "Assisstant",
+        instructions = "You are a study tutor"
+    )
