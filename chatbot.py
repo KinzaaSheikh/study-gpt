d2d7e17bdc53612e7662268a95ac0c3325cd0582
Kinza <kinzasheikhsiddiqui@gmail.com>
2025-09-18 00:22:46 +0500

    LiteLLM module fixed but now Chainlit is throwing errors

--- chatbot.py
+++ chatbot.py
@@ -2,18 +2,21 @@ import os
 from dotenv import load_dotenv
 import chainlit as cl
 from agents import Agent, Runner, set_tracing_disabled
-from agents.extensions.models import litellm_provider, litellm_model
+from agents.extensions.models.litellm_model import LitellmModel
 
 
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
 
-model = litellm_provider()
-
-@cl.on_chat_start
+@cl.on_app_startup()
 async def start():
     agent = Agent(
         name = "Assisstant",
-        instructions = "You are a study tutor"
+        instructions = "You are a study tutor",
+        model = LitellmModel(
+            model="gemini/gemini-1.5-flash",
+            base_url="https://generativelanguage.googleapis.com",
+            api_key=api_key
+        )
     )
